0f4964118be48ebf7308a5517e234881e926bc1f
kritiganotra <kritiganotra@gmail.com>
2025-01-28 19:29:01 +0000

    Finished challenge

--- chat.rb
+++ chat.rb
@@ -6,19 +6,35 @@ client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
 message_list = [
   {
     "role" => "system",
-    "content" => "You are a helpful assistant who talks like Shakespeare."
-  },
-  {
-    "role" => "user",
-    "content" => "Hello! What are the best spots for pizza in Chicago?"
+    "content" => "You are a helpful assistant."
   }
 ]
 
-api_response = client.chat(
-  parameters: {
-    model: "gpt-3.5-turbo",
-    messages: message_list
-  }
-)
+input = ""
 
-pp api_response
+while input != "bye" 
+  puts "Hello! How can I help you today"
+  puts "-" * 50
+
+  input = gets.chomp
+  puts "\n"
+
+  if input != "bye"
+    message_list.push({"role" => "user", "content" => input})
+
+    api_response = client.chat(
+      parameters: {
+        model: "gpt-3.5-turbo",
+        messages: message_list
+        }
+      )
+
+      response = api_response["choices"][0]["message"]["content"]
+      puts response 
+      puts "-" * 50
+
+      message_list.push ({"role" => "assistant", "content" => response})
+  end
+end
+
+puts "Goodbye!"
