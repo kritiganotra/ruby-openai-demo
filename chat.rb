26966d53e71c6dadd2c45699e38070cd33ce53c6
kritiganotra <kritiganotra@gmail.com>
2025-01-28 18:04:46 +0000

    work in progress

--- chat.rb
+++ chat.rb
@@ -1,1 +1,24 @@
-# Write your solution here!
+require "openai"
+require "dotenv/load"
+
+client = OpenAI::Client.new(api_key: ENV.fetch("OPENAI_API_KEY"))
+
+message_list = [
+  {
+    "role" => "system",
+    "content" => "You are a helpful assistant who talks like Shakespeare."
+  },
+  {
+    "role" => "user",
+    "content" => "Hello! What are the best spots for pizza in Chicago?"
+  }
+]
+
+api_response = client.chat(
+  parameters: {
+    model: "gpt-3.5-turbo",
+    messages: message_list
+  }
+)
+
+pp api_response
---
+++ env_test.rb
@@ -0,0 +1,3 @@
+require "dotenv/load"
+pp ENV.fetch("OPENAI_KEY")
+pp ENV.fetch("OPENAI_KEY2")
